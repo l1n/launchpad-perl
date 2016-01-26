9e363a3eb90eeea4cadf16d2a5c14c2a9719e227
l1n <lin@anticlack.com>
2016-01-25 20:55:03 -0500

    Using proper constants

--- launchpad.pl
+++ launchpad.pl
@@ -14,18 +14,39 @@ MIDI::ALSA::start() or die "Can't start MIDIing: $!";
 
 MIDI::ALSA::output(MIDI::ALSA::noteonevent(0xB0, 0x00, 0x00));
 
+my $color = 0b00010001;
+
 while (1) {
     my @alsaevent = MIDI::ALSA::input();
     my @data = @{$alsaevent[7]};
+    my @status = MIDI::ALSA::status();
     if ($#data == 5 && $data[5] == 3) {
         print "Text looped\n";
-    } elsif ($data[2] == 0x7F) {
+    } elsif ($alsaevent[0] == SND_SEQ_EVENT_NOTEON() && !is_noteoff(@alsaevent)) {
+        print "@data\n";
         printf "KeyDown %08B\n", $data[1];
-        MIDI::ALSA::output(MIDI::ALSA::noteevent(0, $data[1], 0x0F, 0, 1));
-    } elsif ($data[2] == 0x00) {
+        if (($data[1] & 0b00001111) == 0b1000) {
+            my $byte = ($data[1] & 0b11110000) / 8 + 1;
+            $color = ($byte & 0b1100) * 4 + ($byte & 0b0011) + 0x0C;
+            printf "Color   %08B\n", $color;
+        } else {
+            MIDI::ALSA::output(MIDI::ALSA::noteonevent(0, $data[1], $color, $status[1]));
+        }
+    } elsif ($alsaevent[0] == SND_SEQ_EVENT_NOTEOFF() || $alsaevent[0] == SND_SEQ_EVENT_NOTEON() && is_noteoff(@alsaevent)) {
         printf "KeyUp   %08B\n", $data[1];
+        MIDI::ALSA::output(MIDI::ALSA::noteoffevent(0, $data[1], 0x00, $status[1]+1));
     } else {
         print "@data\n";
     }
-    print MIDI::ALSA::status();
+}
+
+sub is_noteoff { my @alsaevent = @_;
+    if ($alsaevent[0] == MIDI::ALSA::SND_SEQ_EVENT_NOTEOFF()) {
+        return 1;
+    }
+    if ($alsaevent[0] == MIDI::ALSA::SND_SEQ_EVENT_NOTEON()
+            and $alsaevent[7][2] == 0) {
+        return 1;
+    }
+    return 0;
 }
