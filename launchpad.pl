f61af51680193e529f2e6d9c7a530e519d5187dd
Nova <nova.dassarma@nih.gov>
2018-06-13 14:46:11 -0400

    Three part open

--- launchpad.pl
+++ launchpad.pl
@@ -7,7 +7,7 @@ use local::lib 'perl5';
 
 my $device = shift || 'Launchpad S'; # TODO sanitize
 my($tx, $rx);
-open($tx, "| ./sendmidi dev '$device'") || die "Could not open TX pipe: $!";
-open($rx, "./receivemidi dev 'device' |") || die "Could not open RX pipe: $!";
+open($tx, "|-", "./sendmidi", "dev", $device) || die "Could not open TX pipe: $!";
+open($rx, "-|", "./receivemidi", "dev", $device) || die "Could not open RX pipe: $!";
 close($tx);
 close($rx);
