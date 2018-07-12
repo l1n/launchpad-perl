fc92693d172b9c077b74893ec5f6675d0208007f
Nova <nova.dassarma@nih.gov>
2018-07-12 12:49:42 -0400

    BitBar: Color menu items and don't print checks for good stuff (no news is good news)

--- bitbar-plugin/nagios-status.1m.pl
+++ bitbar-plugin/nagios-status.1m.pl
@@ -4,20 +4,26 @@ use File::Spec;
 my $dirname = File::Spec->rel2abs(dirname(__FILE__));
 system "nohup sh $dirname/../nagios-notify.sh >/dev/null &" unless `ps | grep 'nagios-notify\\.sh'`;
 my $max_length = 0;
+my $menu = "";
+# TODO Rewrite this using GD to create a bas64 encoded png with smooth color scalinga a lot smaller
 my @hostgroups = split /\n/, `fgrep -vf $dirname/../nagios-exclude.list $dirname/../nagios-status.tsv`;
 foreach (@hostgroups) {
+	chomp;
 	/^(.*)\t(\d+)/;
 	$max_length = $max_length > length $1 ? $max_length : length $1;
 	my $bads = $2;
 	if ($bads == 0) {
-		print ":ballot_box_with_check: ";
+		$_ .= "\t color=green";
 	} elsif ($bads > 20) {
 		print ":red_circle: ";
+		$_ .= "\t color=red";
 	} elsif ($bads > 10) {
 		print ":large_orange_diamond: ";
+		$_ .= "\t color=orange";
 	} elsif ($bads > 0) {
 		print ":small_orange_diamond: ";
+		$_ .= "\t color=yellow";
 	}
 }
 print "\n---\n";
-print map {chomp; /^(.*)\t(.*)/; sprintf "%-${max_length}s %03d | font=Courier href=https://nagios.ncbi.nlm.nih.gov/nagios/cgi/status.cgi?style=overview&hostgroup=%s\n", $1, $2, $1} @hostgroups;
+print map {chomp; /^(.*)\t(.*)\t(.*)/; sprintf "%-${max_length}s %03d | font=Courier href=https://nagios.ncbi.nlm.nih.gov/nagios/cgi/status.cgi?style=overview&hostgroup=%s%s\n", $1, $2, $1, $3} @hostgroups;
