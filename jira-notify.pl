06f7113862506c5b5a5eb9a65fa94ec7ea2aef28
Nova <nova.dassarma@nih.gov>
2018-07-12 12:28:47 -0400

    JIRA search updates

--- jira-notify.pl
+++ jira-notify.pl
@@ -1,6 +1,7 @@
 #!/bin/perl
 # Script must be run as JIRA user
 # .jirarc file contains one line with a HTTP Basic Auth token
+# Open tickets is dassarmas2 filter 30311 `010 Unresolved Assigned Tickets'
 
 use strict;
 use warnings;
@@ -16,11 +17,11 @@ chomp $auth;
 while (1) {
 	my $buf;
 	$buf .= "OPEN\t";
-	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status!=Closed+order+by+priority,+updatedDate+DESC'];
+	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status!=Closed+AND+component!="Unix+Projects"+order+by+updatedDate+DESC'];
 	$buf .= "\n";
 	sleep 60 unless $first;
 	$buf .= "CLOSED\t";
-	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status=Closed+order+by+updatedDate+DESC'];
+	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status=Closed+AND+component!="Unix+Projects"+order+by+updatedDate+DESC'];
 	$buf .= "\n";
 	print $buf;
 	STDOUT->flush;
