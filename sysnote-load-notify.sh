a8d64dc8e6c9449810ee9227a1ae68cd0ee8fe48
Nova <nova.dassarma@nih.gov>
2018-07-12 12:31:46 -0400

    Sysnote new notifier

--- sysnote-load-notify.sh
+++ sysnote-load-notify.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-USER=$1
 while true
 do
-ssh irbdev12 '/opt/systems/bin/sysnote_search -u '$USER' --no-color | tail -n +4 | head -n -1 | wc -l'
+ssh irbdev12 '/opt/systems/bin/sysnote_search -u $USER --no-color | tail -n +4 | head -n -1 | wc -l' 2> /dev/null # Redirect STDERR to quash "No result found" messages
+ssh irbdev12 '/opt/systems/bin/sysnote_search --no-color | tail -n +4 | head -n -1' > sysnote.db
+perl -pe 's/^ *([^ ]+) .*$/$1/' sysnote.db > sysnote-host.list
 sleep 10;
 done
