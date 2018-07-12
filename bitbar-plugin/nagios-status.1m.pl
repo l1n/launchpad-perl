a0842cb2c5dd497107170b4fc33f047de1d6dbc2
Nova <nova.dassarma@nih.gov>
2018-07-12 12:31:01 -0400

    Nagios updates including bitbar plugin

---
+++ bitbar-plugin/nagios-status.1m.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use File::Basename;
+use File::Spec;
+my $dirname = File::Spec->rel2abs(dirname(__FILE__));
+system "nohup sh $dirname/../nagios-notify.sh >/dev/null &" unless `ps | grep 'nagios-notify\\.sh'`;
+my $max_length = 0;
+my @hostgroups = split /\n/, `fgrep -vf $dirname/../nagios-exclude.list $dirname/../nagios-status.tsv`;
+foreach (@hostgroups) {
+	/^(.*)\t(\d+)/;
+	$max_length = $max_length > length $1 ? $max_length : length $1;
+	my $bads = $2;
+	if ($bads == 0) {
+		print ":ballot_box_with_check: ";
+	} elsif ($bads > 50) {
+		print ":warning: ";
+	} elsif ($bads > 20) {
+		print ":red_circle: ";
+	} elsif ($bads > 10) {
+		print ":large_orange_diamond: ";
+	} elsif ($bads > 0) {
+		print ":small_orange_diamond: ";
+	}
+}
+print "\n---\n";
+print map {chomp; /^(.*)\t(.*)/; sprintf "%-${max_length}s %03d | font=Courier href=https://nagios.ncbi.nlm.nih.gov/nagios/cgi/status.cgi?style=overview&hostgroup=$s\n", $1, $2, $1} @hostgroups;
--- nagios-notify.sh
+++ nagios-notify.sh
@@ -2,5 +2,5 @@
 while true
 do
 curl -s --negotiate -u : 'https://nagios.ncbi.nlm.nih.gov/nagios/cgi/status.cgi?hostgroup=all&style=overview' -o nagios-status.html
-egrep 'hostgroup=|miniStatusCRITICAL|miniStatusWarning|miniStatusOK' nagios-status.html | grep -v hostgroup=all | perl nagios-notify.pl | grep -vE 'gc-va\t' | perl nagios-notify.pl | tee nagios-status.tsv
+egrep 'hostgroup=|miniStatusCRITICAL|miniStatusWarning|miniStatusOK' nagios-status.html | fgrep -v hostgroup=all | fgrep -vf sysnote-host.list | fgrep -vf nagios-exclude.list | perl nagios-notify.pl | grep -vE 'gc-va\t' | perl nagios-notify.pl | tee nagios-status.tsv
 done
