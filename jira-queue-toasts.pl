16d1f9d0c8930dc28b41fc5cd8a981a4835678ea
Nova <nova.dassarma@nih.gov>
2019-01-30 12:50:32 -0500

    Jira scripts swapped

--- jira-queue-toasts.pl
+++ jira-queue-toasts.pl
@@ -1,12 +1,13 @@
 #!/bin/perl
 # Script must be run as JIRA user
 # .jirarc file contains one line with a HTTP Basic Auth token
-# Open tickets is dassarmas2 filter 30311 `010 Unresolved Assigned Tickets'
+# Ticket source is dassarmas2 filter 30407 `Unix Q'
 
 use strict;
 use warnings;
 
 use IO::Handle;
+use JSON::PP qw(decode_json);
 
 autoflush STDOUT 0;
 my $first = 1;
@@ -14,17 +15,17 @@ my $first = 1;
 my $user = $ENV{USER};
 my $auth = `cat .jirarc`;
 chomp $auth;
-while (1) {
-	my $buf;
-	$buf .= "OPEN\t";
-	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status!=Closed+AND+component!="Unix+Projects"+order+by+updatedDate+DESC'];
-	$buf .= "\n";
-	sleep 60 unless $first;
-	$buf .= "CLOSED\t";
-	$buf .= qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=assignee='$user'+AND+status=Closed+AND+component!="Unix+Projects"+order+by+updatedDate+DESC'];
-	$buf .= "\n";
-	print $buf;
-	STDOUT->flush;
-	sleep 60;
-	$first = 0;
-}
+my $i = 0;
+my $last_ticket_count = 0;
+my $current_ticket_count = 0;
+do {
+	$current_ticket_count = qx[curl -s --user-agent 'Intequeo/0.1' -H 'Authorization: Basic $auth' 'https://jira.ncbi.nlm.nih.gov/rest/api/2/search?jql=project+=+SYS+AND+component+=+UNIX+AND+assignee+is+EMPTY+AND+(status+=+Open+OR+status+=+New)+ORDER+BY+created+DESC'];
+	$current_ticket_count = decode_json($current_ticket_count)->{'total'};
+	if ($i) {
+		if ($current_ticket_count > $last_ticket_count) {
+			system "osascript ffjiranotify.scpt";
+		}
+		sleep 30;
+	}
+	$last_ticket_count = $current_ticket_count;
+} while (++$i);
